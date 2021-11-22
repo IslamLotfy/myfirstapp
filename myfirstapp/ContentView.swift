55c2cc1629b14f217335b58c93a24f1187cd40fd
islamlotfy <islam.m7md.lotfy@gmail.com>
2021-11-22 21:38:03 +0200

    Initial Commit

---
+++ myfirstapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myfirstapp
+//
+//  Created by Islam Lotfy on 22/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myfirstapp/myfirstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfirstappApp.swift
+//  myfirstapp
+//
+//  Created by Islam Lotfy on 22/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct myfirstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
